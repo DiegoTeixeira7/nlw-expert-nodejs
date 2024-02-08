76cd416952ffb79533e2716ffc18b03baa12a16a
DiegoTeixeira7 <fadetoblacksdi07ego>
2024-02-08 08:16:49 -0300

    ✨ feat: added files classroom 3

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,12 +6,17 @@
 // npx prisma migrate dev
 // npx prisma studio
 
+// https://hoppscotch.io/
+
 import fastify from "fastify";
-import cookie from "@fastify/cookie";
+import cookie from "@fastify/cookie"
+import websocket from "@fastify/websocket";;
 
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
+
 const app = fastify()
 
 app.register(cookie, {
@@ -19,9 +24,13 @@ app.register(cookie, {
   hook: 'onRequest',
 })
 
+app.register(websocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!")
